e73e2c6c906dd46689c7d82ff1e7630274f62517
karl <karol010k@gmail.com>
2022-07-15 00:10:33 +0200

    Addded database

---
+++ utils/db.ts
@@ -0,0 +1,9 @@
+import {createPool} from "mysql2/promise";
+
+export const pool = createPool({
+    host: 'localhost',
+    user:'root',
+    database: 'list_products',
+    namedPlaceholders: true,
+    decimalNumbers: true,
+});
