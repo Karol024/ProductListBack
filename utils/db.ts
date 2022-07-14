3afa8708b32449b39c5c5737cf17d843a2472550
Karol024 <105817380+Karol024@users.noreply.github.com>
2022-07-15 00:17:12 +0200

    Delete utils directory

--- utils/db.ts
+++
@@ -1,9 +0,0 @@
-import {createPool} from "mysql2/promise";
-
-export const pool = createPool({
-    host: 'localhost',
-    user:'root',
-    database: 'list_products',
-    namedPlaceholders: true,
-    decimalNumbers: true,
-});
